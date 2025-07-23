ccb7b2f7e68d26f446afff7afd6a4d4b54a78204
ClementSam <58741875+ClementSam@users.noreply.github.com>
2025-07-23 23:47:59 +0200

    Update __init__.py

---
+++ IO_dossier/__init__.py
@@ -0,0 +1,12 @@
+"""VISA communication interface for SCPI-based instrument remote control.
+    :version: 1.19.0.75
+    :copyright: 2020 by Rohde & Schwarz GMBH & Co. KG
+    :license: MIT, see LICENSE for more details.
+"""
+
+
+__version__ = 'x.x'
+
+# Main class
+from RTxReadBin import RTxReadBin
+
